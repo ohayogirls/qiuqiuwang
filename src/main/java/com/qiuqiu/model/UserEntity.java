ddf58b4c3189fc8f25d814aec142c9209c5ed017
ohayogirls <530317349@qq.com>
2017-12-15 17:21:46 +0800

    qiuqiuwang second step of MySQL

---
+++ src/main/java/com/qiuqiu/model/UserEntity.java
@@ -0,0 +1,70 @@
+package com.qiuqiu.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user", schema = "qiuqiuwang", catalog = "")
+public class UserEntity {
+    private int id;
+    private String username;
+    private String password;
+    private String sex;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 255)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 255)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "sex", nullable = false, length = 255)
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return id == that.id &&
+                Objects.equals(username, that.username) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(sex, that.sex);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, username, password, sex);
+    }
+}
